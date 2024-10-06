2204c0a36fa5a67ea4dd25b2f048265674464d46
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-10-06 08:07:41 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup, find_packages
 
-
-def parse_requirements(filename):
-    with open(filename, 'r') as file:
-        return [line.strip() for line in file if line.strip() and not line.startswith("#")]
-
-
 setup(
     name="doc-scanner",
     version="1.0.0-alpha",
     packages=find_packages(),
-    install_requires=parse_requirements("requirements.txt"),
+    install_requires=[
+        "imageio>=2.35.1",
+        "imutils>=0.5.4",
+        "lazy_loader>=0.4",
+        "networkx>=3.3",
+        "numpy>=2.1.1",
+        "opencv-python>=4.10.0.84",
+        "packaging>=24.1",
+        "pillow>=10.4.0",
+        "scikit-image>=0.24.0",
+        "scipy>=1.14.1",
+        "setuptools>=75.1.0",
+        "tifffile>=2024.9.20"
+    ],
     author="Makrosoft",
     author_email="devjerzy@gmail.com",
     description="Package for scanning documents",
