fd736c088811984155ee1e9a05c9d1a9cf15419d
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-10-06 08:35:21 +0200

    rename

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="doc-scanner",
+    name="doc_scanner",
     version="1.0.0-alpha",
     packages=find_packages(),
     install_requires=[
